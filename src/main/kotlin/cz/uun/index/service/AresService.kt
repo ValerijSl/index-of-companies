8cb9de8edbefe93475454b48cd7b3ca62606302d
Valerij Slovikov <c_vslovikov@groupon.com>
2024-06-02 14:35:03 +0200

    post to ares

--- src/main/kotlin/cz/uun/index/service/AresService.kt
+++ src/main/kotlin/cz/uun/index/service/AresService.kt
@@ -3,6 +3,7 @@ package cz.uun.index.service
 import cz.uun.index.config.ApiUrlConfig
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.stereotype.Service
+import org.springframework.web.reactive.function.BodyInserters
 import org.springframework.web.reactive.function.client.WebClient
 
 @Service
@@ -20,6 +21,24 @@ class AresService(
             it.sidlo.get("textovaAdresa")?.let { it1 -> CompanyInfo(it.ico, it.obchodniJmeno, it1) }
         }
         .block()
+
+    fun postCompanyByName(name: String, address: String, count: Int = 100): CompanyInfo? {
+        return webClient.post()
+                .uri(apiUrlConfig.getByIcoUrl) // Confirm this URL is correct for your POST request
+                .body(BodyInserters.fromValue(mapOf(
+                        "pocet" to count,
+                        "obchodniJmeno" to name,
+                        "sidlo" to mapOf("textovaAdresa" to address)
+                )))
+                .retrieve()
+                .bodyToMono(CompanyInfoResponse::class.java)
+                .mapNotNull {
+                    it.sidlo["textovaAdresa"]?.let { addr ->
+                        CompanyInfo(it.ico, it.obchodniJmeno, addr)
+                    }
+                }
+                .block()
+    }
 }
 
 data class CompanyInfoResponse(
